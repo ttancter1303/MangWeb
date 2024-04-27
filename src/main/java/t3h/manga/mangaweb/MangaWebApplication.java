a098d5b8c015bb33f6110c3e57eac73a9cefd297
ttancter1303 <01689674504zx@gmail.com>
2024-04-27 20:54:27 +0700

    first commit

---
+++ src/main/java/t3h/manga/mangaweb/MangaWebApplication.java
@@ -0,0 +1,13 @@
+package t3h.manga.mangaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/t3h/manga/mangaweb/MangaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package t3h.manga.mangaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
