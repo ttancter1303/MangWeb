d22249ab748c4eac90899fb0ef014f1183edf340
ttancter1303 <01689674504zx@gmail.com>
2024-05-17 02:51:26 +0700

    feat(backend): add Backend ui,table manga, add search method

--- src/main/java/t3h/manga/mangaweb/controller/AdminController.java
+++ src/main/java/t3h/manga/mangaweb/controller/AdminController.java
@@ -1,5 +1,6 @@
 package t3h.manga.mangaweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import t3h.manga.mangaweb.model.Manga;
+import t3h.manga.mangaweb.repository.MangaRepository;
 
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-    @GetMapping("/")
+    @Autowired
+    MangaRepository mangaRepository;
+    @GetMapping("/dashboard")
     public String getDashboard(HttpSession session, Model model) {
 
         model.addAttribute("title", "Dashboard");
@@ -23,6 +29,33 @@ public class AdminController {
         model.addAttribute("content", "backend/index.html");
 
         return "layouts/adminlte3.html";
+    }
+    @GetMapping("/")
+    public String getAdminPage(HttpSession session, Model model) {
+        List<Manga> mangas = mangaRepository.findAll();
+        model.addAttribute("title", "Page");
+        model.addAttribute("mangas", mangas);
+        model.addAttribute("content", "backend/mangas.html");
+        return "layouts/adminlte3";
+    }
+    @GetMapping("/mangas/search")
+    public String searchMangas(@RequestParam(required = false) String name,
+                               @RequestParam(required = false) String tag,
+                               Model model) {
+        List<Manga> mangas;
+
+        if ((name != null && !name.isEmpty()) && (tag != null && !tag.isEmpty())) {
+            mangas = mangaRepository.findByNameContainingAndTagName(name, tag);
+        } else if (name != null && !name.isEmpty()) {
+            mangas = mangaRepository.findMangaByName(name);
+        } else if (tag != null && !tag.isEmpty()) {
+            mangas = mangaRepository.findByTagName(tag);
+        } else {
+            mangas = mangaRepository.findAll();
+        }
+
+        model.addAttribute("mangas", mangas);
+        return "backend/mangas";
     }
     
 }
--- src/main/java/t3h/manga/mangaweb/repository/MangaRepository.java
+++ src/main/java/t3h/manga/mangaweb/repository/MangaRepository.java
@@ -1,13 +1,22 @@
 package t3h.manga.mangaweb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import t3h.manga.mangaweb.model.Manga;
 import java.util.List;
 import java.util.Optional;
 
 
 public interface MangaRepository extends JpaRepository<Manga,Integer> {
-    Manga findMangaByName(String name);
-    
+    List<Manga> findMangaByName(String name);
+
+    @Query("SELECT m FROM Manga m JOIN m.listTag t WHERE t.name = :tag")
+    List<Manga> findByTagName(@Param("tag") String tag);
+
+    @Query("SELECT m FROM Manga m JOIN m.listTag t WHERE m.name LIKE %:name% AND t.name = :tag")
+    List<Manga> findByNameContainingAndTagName(@Param("name") String name, @Param("tag") String tag);
+
+
     Optional<Manga> findBySource(String source);
 }
