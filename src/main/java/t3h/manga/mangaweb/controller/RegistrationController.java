f55a456ec78277f4a96170d88e301e3e4ba4dc4d
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-05-23 14:17:26 +0700

    feat(create fe): merge overwritten

---
+++ src/main/java/t3h/manga/mangaweb/controller/RegistrationController.java
@@ -0,0 +1,41 @@
+package t3h.manga.mangaweb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import t3h.manga.mangaweb.model.Account;
+import t3h.manga.mangaweb.repository.AccountRepository;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("account", new Account());
+        model.addAttribute("title", "register");
+        model.addAttribute("content", "frontend/register.html");
+        return "frontend/index.html";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute Account account, Model model) {
+        if (accountRepository.findAccountByUsername(account.getUsername()) != null) {
+            model.addAttribute("error", "Username already exists!");
+            return "frontend/register";
+        }
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
+        account.setRole("USER");
+        accountRepository.save(account);
+        return "redirect:/login";
+    }
+}
