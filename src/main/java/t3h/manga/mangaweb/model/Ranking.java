980fff34036643882d47c6791684703515adfe62
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-05-28 18:57:10 +0700

    feat(create fe): create reading page and create favor, history and ranking model

---
+++ src/main/java/t3h/manga/mangaweb/model/Ranking.java
@@ -0,0 +1,29 @@
+package t3h.manga.mangaweb.model;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+public class Ranking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    @Column(nullable = true)
+    private List<Manga> day;
+    
+    @Column(nullable = true)
+    private List<Manga> week;
+    
+    @Column(nullable = true)
+    private List<Manga> month;
+
+}
