dd7637a3a77362bb18963169f22b6204b35ed9a4
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-05-28 23:04:57 +0700

    feat(create fe): add cmt with facebook, use ngrok to change to subdomain

--- src/main/java/t3h/manga/mangaweb/model/History.java
+++ src/main/java/t3h/manga/mangaweb/model/History.java
@@ -1,33 +1,33 @@
-package t3h.manga.mangaweb.model;
+// package t3h.manga.mangaweb.model;
 
-import java.util.List;
+// import java.util.List;
 
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+// import jakarta.persistence.*;
+// import lombok.Data;
+// import lombok.Getter;
+// import lombok.Setter;
 
 
-@Entity
-@Data
-@Getter
-@Setter
-public class History {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+// @Entity
+// @Data
+// @Getter
+// @Setter
+// public class History {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Integer id;
 
-    @OneToOne
-    private Account user;
+//     @OneToOne
+//     private Account user;
 
-    @ManyToMany
-    @JoinTable(
-        name="history_manga",
-        joinColumns = @JoinColumn(name="history_id"),
-        inverseJoinColumns = @JoinColumn(name = "manga_id")
-    )
-    private List<Manga> listMangas;
+//     @ManyToMany
+//     @JoinTable(
+//         name="history_manga",
+//         joinColumns = @JoinColumn(name="history_id"),
+//         inverseJoinColumns = @JoinColumn(name = "manga_id")
+//     )
+//     private List<Manga> listMangas;
 
-    @Column(nullable = true)
-    private Chapter lastestChapter;
-}
+//     @Column(nullable = true)
+//     private Chapter lastestChapter;
+// }
