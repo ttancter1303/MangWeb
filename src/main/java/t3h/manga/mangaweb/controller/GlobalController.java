ceb25dadb2b6af896ab05d61a9c4248c651c468e
vuvuvuvvv <nguyenvu260502@gmail.com>
2024-05-30 07:04:45 +0700

    feat(create-oauth): add method to get custom properties in appli...properties

--- src/main/java/t3h/manga/mangaweb/controller/GlobalController.java
+++ src/main/java/t3h/manga/mangaweb/controller/GlobalController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import t3h.manga.mangaweb.components.CustomProperties;
 import t3h.manga.mangaweb.service.TagService;
 
 @ControllerAdvice
@@ -13,11 +15,15 @@ public class GlobalController {
     @Autowired
     private TagService tagService;
 
+    @Autowired
+    private CustomProperties customProperties;
+
     @ModelAttribute("global")
     public HashMap<String, Object> getGlobalData() {
         HashMap<String, Object> global = new HashMap<>();
         global.put("tags", tagService.getAllTags());
         global.put("appname", "NetSteal");
+        global.put("baseUrl", customProperties.getBaseUrl());
         return global;
     }
 }
